1fec8e49891de4a643a21b9a083508008dde2354
Advik <advik.b@gmail.com>
2023-09-27 22:36:13 +0530

    Update colour.py

--- pseudoku/classes/colour.py
+++ pseudoku/classes/colour.py
@@ -17,3 +17,58 @@ class Colour:
     @staticmethod
     def from_dict(colour_dict: dict[str, str]) -> "Colour":
         return Colour.from_hex(colour_dict["hex"])
+
+    # @staticmethod
+    # def from_hsv(hsv: tuple[int, int, int]) -> "Colour":
+    #     h, s, v = hsv
+    #     c = v * s
+    #     x = c * (1 - abs((h / 60) % 2 - 1))
+    #     m = v - c
+    #
+    #     if h < 60:
+    #         r, g, b = c, x, 0
+    #     elif h < 120:
+    #         r, g, b = x, c, 0
+    #     elif h < 180:
+    #         r, g, b = 0, c, x
+    #     elif h < 240:
+    #         r, g, b = 0, x, c
+    #     elif h < 300:
+    #         r, g, b = x, 0, c
+    #     elif h < 360:
+    #         r, g, b = c, 0, x
+    #     else:
+    #         # Subtract 360 from h until it is in the range [0, 360)
+    #         while h >= 360:
+    #             h -= 360
+    #         return Colour.from_hsv((h, s, v))
+    #
+    #     return Colour.from_rgb(tuple(int((c + m) * 255) for c in (r, g, b)))
+
+    # def COMPLEMENTARY(self) -> "Colour":
+    #     return Colour.from_rgb(tuple(255 - c for c in self.rgb))
+    #
+    # def to_hsv(self) -> tuple[int, int, int]:
+    #     r, g, b = (c / 255 for c in self.rgb)
+    #     c_max = max(r, g, b)
+    #     c_min = min(r, g, b)
+    #     delta = c_max - c_min
+    #
+    #     if delta == 0:
+    #         h = 0
+    #     elif c_max == r:
+    #         h = 60 * (((g - b) / delta) % 6)
+    #     elif c_max == g:
+    #         h = 60 * (((b - r) / delta) + 2)
+    #     elif c_max == b:
+    #         h = 60 * (((r - g) / delta) + 4)
+    #
+    #     s = 0 if c_max == 0 else delta / c_max
+    #     v = c_max
+    #
+    #     return h, s, v
+    #
+    # def TRIAD(self) -> tuple["Colour", "Colour"]:
+    #     h, s, v = self.to_hsv()
+    #     print(h, s, v)
+    #     return Colour.from_hsv((h + 120, s, v)), Colour.from_hsv((h + 240, s, v))
