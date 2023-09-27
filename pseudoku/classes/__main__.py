53216fe33befbec93213463414816daa587a1299
Advik <advik.b@gmail.com>
2023-09-27 22:35:46 +0530

    Update __main__.py

--- pseudoku/classes/__main__.py
+++ pseudoku/classes/__main__.py
@@ -2,5 +2,12 @@ from .colour import Colour
 
 navy_blue = Colour.from_hex("#3900E3")
 
+light_bg = Colour.from_hex("#b796f8")
+light_comp = Colour.from_hex("#aae300")
+light_triad = Colour.from_hex("#e33900")
 
 print(navy_blue)
+print(light_comp)
+print(light_bg)
+print(light_triad)
+
