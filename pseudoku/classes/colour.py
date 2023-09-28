57689b0fe5d9ee16b541f5596d02382652f14cdf
Advik <advik.b@gmail.com>
2023-09-28 20:07:38 +0530

    Update colour.py

--- pseudoku/classes/colour.py
+++ pseudoku/classes/colour.py
@@ -4,78 +4,97 @@ from dataclasses import dataclass
 class Colour:
     rgb: tuple[int, int, int]
     hex: str
+    hsv: tuple[int, int, int] = None
 
     def __post_init__(self):
         self.hex = self.hex.upper()
 
     @staticmethod
     def from_hex(hex: str) -> "Colour":
-        return Colour(tuple(int(hex.casefold()[1:][i:i + 2], 16) for i in (0, 2, 4)), hex.casefold())
+        rgb = tuple(int(hex.casefold()[1:][i:i + 2], 16) for i in (0, 2, 4))
+        return Colour(rgb, hex, Colour.rgb_to_hsv(rgb))
 
     @staticmethod
     def from_rgb(rgb: tuple[int, int, int]) -> "Colour":
-        return Colour(rgb, "#"+"".join(f"{c:02x}" for c in rgb))
+        return Colour(rgb, "#"+"".join(f"{c:02x}" for c in rgb), Colour.rgb_to_hsv(rgb))
 
 
     @staticmethod
     def from_dict(colour_dict: dict[str, str]) -> "Colour":
         return Colour.from_hex(colour_dict["hex"])
 
-    to_hex = lambda self: self.hex
+    @staticmethod
+    def from_hsv(hsv: tuple[int, int, int]) -> "Colour":
+        return Colour.from_rgb(Colour.hsv_to_rgb(hsv))
+
+    @staticmethod
+    def rgb_to_hsv(rgb: tuple[int, int, int]) -> tuple[int, int, int]:
+        """
+        Convert RGB to HSV
+        :param rgb:
+        :return:
+        """
+        r, g, b = rgb
+        r, g, b = r / 255.0, g / 255.0, b / 255.0
+
+        max_val = max(r, g, b)
+        min_val = min(r, g, b)
+        delta = max_val - min_val
+
+        # Calculate Hue
+        if delta == 0:
+            h = 0
+        elif max_val == r:
+            h = ((g - b) / delta) % 6
+        elif max_val == g:
+            h = (b - r) / delta + 2
+        else:
+            h = (r - g) / delta + 4
+
+        h = int(h * 60)
+        if h < 0:
+            h += 360
+
+        # Calculate Saturation
+        s = 0 if max_val == 0 else int((delta / max_val) * 100)
+
+        # Calculate Value
+        v = int(max_val * 100)
+
+        return h, s, v
+
+
+    @staticmethod
+    def hsv_to_rgb(hsv: tuple[int, int, int]) -> tuple[int, int, int]:
+        h, s, v = hsv
+        h /= 360.0
+        s /= 100.0
+        v /= 100.0
+
+        i = int(h * 6)
+        f = (h * 6) - i
+        p = v * (1 - s)
+        q = v * (1 - f * s)
+        t = v * (1 - (1 - f) * s)
+
+        if i % 6 == 0:
+            r, g, b = v, t, p
+        elif i % 6 == 1:
+            r, g, b = q, v, p
+        elif i % 6 == 2:
+            r, g, b = p, v, t
+        elif i % 6 == 3:
+            r, g, b = p, q, v
+        elif i % 6 == 4:
+            r, g, b = t, p, v
+        else:
+            r, g, b = v, p, q
+
+        r = int(r * 255)
+        g = int(g * 255)
+        b = int(b * 255)
+
+        return r, g, b
+
     to_rgb = lambda self: self.rgb
     to_dict = lambda self: {"hex": self.hex, "rgb": self.rgb}
-
-    # @staticmethod
-    # def from_hsv(hsv: tuple[int, int, int]) -> "Colour":
-    #     h, s, v = hsv
-    #     c = v * s
-    #     x = c * (1 - abs((h / 60) % 2 - 1))
-    #     m = v - c
-    #
-    #     if h < 60:
-    #         r, g, b = c, x, 0
-    #     elif h < 120:
-    #         r, g, b = x, c, 0
-    #     elif h < 180:
-    #         r, g, b = 0, c, x
-    #     elif h < 240:
-    #         r, g, b = 0, x, c
-    #     elif h < 300:
-    #         r, g, b = x, 0, c
-    #     elif h < 360:
-    #         r, g, b = c, 0, x
-    #     else:
-    #         # Subtract 360 from h until it is in the range [0, 360)
-    #         while h >= 360:
-    #             h -= 360
-    #         return Colour.from_hsv((h, s, v))
-    #
-    #     return Colour.from_rgb(tuple(int((c + m) * 255) for c in (r, g, b)))
-
-    # def COMPLEMENTARY(self) -> "Colour":
-    #     return Colour.from_rgb(tuple(255 - c for c in self.rgb))
-    #
-    # def to_hsv(self) -> tuple[int, int, int]:
-    #     r, g, b = (c / 255 for c in self.rgb)
-    #     c_max = max(r, g, b)
-    #     c_min = min(r, g, b)
-    #     delta = c_max - c_min
-    #
-    #     if delta == 0:
-    #         h = 0
-    #     elif c_max == r:
-    #         h = 60 * (((g - b) / delta) % 6)
-    #     elif c_max == g:
-    #         h = 60 * (((b - r) / delta) + 2)
-    #     elif c_max == b:
-    #         h = 60 * (((r - g) / delta) + 4)
-    #
-    #     s = 0 if c_max == 0 else delta / c_max
-    #     v = c_max
-    #
-    #     return h, s, v
-    #
-    # def TRIAD(self) -> tuple["Colour", "Colour"]:
-    #     h, s, v = self.to_hsv()
-    #     print(h, s, v)
-    #     return Colour.from_hsv((h + 120, s, v)), Colour.from_hsv((h + 240, s, v))
