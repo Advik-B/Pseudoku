f7dc3bc9722cf61193d9cb5cf13d88070ebdc2ff
Advik <advik.b@gmail.com>
2023-09-28 14:24:54 +0530

    Delete conftest.py

--- conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-def pytest_sessionfinish(session, exitstatus):
-    if exitstatus == pytest.ExitCode.NO_TESTS_COLLECTED:
-        # Modify the exit status to 0 if no tests were run
-        return pytest.ExitCode.OK
