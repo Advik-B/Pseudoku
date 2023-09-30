32675eb1a4bff733f156421d7355ea79b8c3677f
Advik <advik.b@gmail.com>
2023-09-30 20:16:08 +0530

    Create settings.py

---
+++ pseudoku/classes/settings.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from .theme import Theme
+
+@dataclass
+class Settings:
+    width: int
+    height: int
+    theme: Theme
+    vsync: bool
+    fps: int
+
+    # Debug info
+    debug: bool = False
+    version_string: str = "v0.1.0"
+    version_number: tuple[int, int, int] = (0, 1, 0)
+
+
+    @staticmethod
+    def from_dict(settings_dict: dict[str, str]) -> "Settings":
+        return Settings(
+            width=settings_dict["width"],
+            height=settings_dict["height"],
+            theme=Theme.from_dict(settings_dict["theme"]),
+            vsync=settings_dict["vsync"],
+            fps=settings_dict["fps"],
+            debug=settings_dict["debug"],
+            version_string=settings_dict["version_string"],
+            version_number=settings_dict["version_number"]
+        )
+
+    def __post_init__(self):
+        self.width = round(self.width)
+        self.height = round(self.height)
+        self.vsync = bool(self.vsync)
+        self.fps = round(self.fps)
+        self.debug = bool(self.debug)
+        self.version_string = str(self.version_string)
+        self.version_number = tuple(int(i) for i in self.version_number)
