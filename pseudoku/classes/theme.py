6e8a83059ae514f0f45fae5b9719d6fecd9efb0a
Advik <advik.b@gmail.com>
2023-09-30 20:34:46 +0530

    Update theme.py

--- pseudoku/classes/theme.py
+++ pseudoku/classes/theme.py
@@ -38,8 +38,10 @@ class Theme:
     @staticmethod
     def from_dict(theme_dict: dict[str, str]) -> "Theme":
         return Theme(
-            background_colour=Colour.from_dict(theme_dict["background_colour"]),
-            complementary_colour=Colour.from_dict(theme_dict["complementary_colour"]),
+            background_colour=Colour.from_dict(
+                theme_dict["background_colour"]),
+            complementary_colour=Colour.from_dict(
+                theme_dict["complementary_colour"]),
             triad_colour=Colour.from_dict(theme_dict["triad_colour"]),
             text_colour=Colour.from_dict(theme_dict["text_colour"]),
             number_font=Font.from_dict(theme_dict["number_font"]),
@@ -47,17 +49,17 @@ class Theme:
             subtitle_font=Font.from_dict(theme_dict["subtitle_font"])
         )
 
-
     @staticmethod
     @property
     def DEFAULT() -> "Theme":
         return Theme(
-            background_colour=Colour.from_hex(DEFAULT_THEME["background_colour"]),
-            complementary_colour=Colour.from_hex(DEFAULT_THEME["complementary_colour"]),
+            background_colour=Colour.from_hex(
+                DEFAULT_THEME["background_colour"]),
+            complementary_colour=Colour.from_hex(
+                DEFAULT_THEME["complementary_colour"]),
             triad_colour=Colour.from_hex(DEFAULT_THEME["triad_colour"]),
             text_colour=Colour.from_hex(DEFAULT_THEME["text_colour"]),
             number_font=Font.from_dict(DEFAULT_THEME["number_font"]),
             title_font=Font.from_dict(DEFAULT_THEME["title_font"]),
             subtitle_font=Font.from_dict(DEFAULT_THEME["subtitle_font"])
         )
-
