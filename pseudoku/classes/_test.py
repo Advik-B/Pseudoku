b0d4d1e66e58f95f78b531402303dc631c8948c2
Advik <advik.b@gmail.com>
2023-09-30 20:34:38 +0530

    Update _test.py

--- pseudoku/classes/_test.py
+++ pseudoku/classes/_test.py
@@ -1,10 +1,12 @@
 from .colour import Colour
 
+
 def test_rgb_to_hex():
     assert Colour.from_rgb((57, 0, 227)).to_hex() == "#3900E3".upper()
     assert Colour.from_rgb((183, 150, 248)).to_hex() == "#b796f8".upper()
     assert Colour.from_rgb((170, 227, 0)).to_hex() == "#aae300".upper()
     assert Colour.from_rgb((227, 57, 0)).to_hex() == "#e33900".upper()
+
 
 def test_hex_to_rgb():
     assert Colour.from_hex("#3900E3").to_rgb() == (57, 0, 227)
