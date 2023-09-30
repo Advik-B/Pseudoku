27e8c2ea292a2071d63590ea9999cf8f9d09f00f
Advik <advik.b@gmail.com>
2023-09-30 20:34:34 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 def pytest_sessionfinish(session, exitstatus):
     if exitstatus == pytest.ExitCode.NO_TESTS_COLLECTED:
         # Modify the exit status to 0 if no tests were run
