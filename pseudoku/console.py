14903638832d668b29d0ea5d2f2118b1b22382aa
Advik <advik.b@gmail.com>
2023-10-01 11:07:20 +0530

    Update console.py

--- pseudoku/console.py
+++ pseudoku/console.py
@@ -27,12 +27,30 @@ def solve_sudoku(board):
     return True
 
 
-def generate_sudoku():
+def calculate_empty_cells(difficulty):
+    if 1 <= difficulty <= 3:
+        return random.randint(48, 54)
+    elif 4 <= difficulty <= 6:
+        return random.randint(36, 47)
+    elif 7 <= difficulty <= 8:
+        return random.randint(27, 35)
+    elif difficulty == 9:
+        return random.randint(18, 26)
+    elif difficulty == 10:
+        return random.randint(9, 17)
+    else:
+        return random.randint(36, 54)
+
+
+def generate_sudoku(difficulty):
     board = [[0 for _ in range(9)] for _ in range(9)]
     solve_sudoku(board)
 
-    empty_cells = random.sample(range(81), 40)
-    for idx in empty_cells:
+    empty_cells = calculate_empty_cells(difficulty)
+    empty_cells = min(empty_cells, 81)
+    empty_positions = random.sample(range(81), empty_cells)
+
+    for idx in empty_positions:
         row, col = divmod(idx, 9)
         board[row][col] = 0
 
@@ -64,31 +82,61 @@ def is_valid_solution(board):
 
 
 def play_sudoku():
-    sudoku_board = generate_sudoku()
-    print("Generated Sudoku Board:")
+    print("Choose the difficulty level (1 to 10, where 1 is easy and 10 is hardest):")
+    try:
+        difficulty = int(input("Enter the difficulty level: "))
+        if not (1 <= difficulty <= 10):
+            print("Invalid difficulty level. Using default difficulty (medium).")
+            difficulty = 5
+    except ValueError:
+        print("Invalid input. Using default difficulty (medium).")
+        difficulty = 5
+
+    sudoku_board = generate_sudoku(difficulty)
+    print("\nGenerated Sudoku Board (Difficulty Level: {}):".format(difficulty))
     print_board(sudoku_board)
 
     while True:
-        print(
-            "\nEnter row, column, and number (space-separated) to fill a cell (e.g., 3 4 7):"
-        )
-        try:
-            row, col, num = map(int, input().split())
-            if 1 <= row <= 9 and 1 <= col <= 9 and 1 <= num <= 9:
-                if sudoku_board[row - 1][col - 1] == 0:
-                    sudoku_board[row - 1][col - 1] = num
-                    print("\nUpdated Sudoku Board:")
-                    print_board(sudoku_board)
-                else:
-                    print("Cell is already filled. Try again.")
-            else:
-                print("Invalid input. Please enter valid row, column, and number.")
-        except ValueError:
-            print("Invalid input format. Please enter row, column, and number.")
+        print("\nEnter 'q' to quit or 's' to solve with AI:")
+        choice = input().strip().lower()
 
-        if is_valid_solution(sudoku_board):
-            print("\nCongratulations! You solved the Sudoku puzzle.")
+        if choice == "q":
             break
+        elif choice == "s":
+            solve_with_ai(sudoku_board)
+        else:
+            print("Invalid choice. Please try again.")
+
+
+def ai_is_valid(board, row, col, num):
+    return (
+        all(board[row][i] != num for i in range(9))
+        and all(board[i][col] != num for i in range(9))
+        and all(
+            board[row // 3 * 3 + i][col // 3 * 3 + j] != num
+            for i in range(3)
+            for j in range(3)
+        )
+    )
+
+
+def ai_solver(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if ai_is_valid(board, row, col, num):
+                        board[row][col] = num
+                        yield board
+                        yield from ai_solver(board)
+                        board[row][col] = 0
+
+
+def solve_with_ai(board):
+    for solution in ai_solver(board):
+        print("Solving step:")
+        print_board(solution)
+        input("Press Enter to continue...")
 
 
 if __name__ == "__main__":
