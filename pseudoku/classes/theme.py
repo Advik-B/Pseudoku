085abec69d8fbc58f035d32a3e3a926e9c16b262
Advik <advik.b@gmail.com>
2023-10-01 10:10:48 +0530

    Update theme.py

--- pseudoku/classes/theme.py
+++ pseudoku/classes/theme.py
@@ -4,10 +4,10 @@ from .font import Font
 
 ROBOTO = "assets/fonts/Roboto/Roboto-Regular.ttf"
 DEFAULT_THEME = {
-    "background_colour": {"hex": "#b796f8", "rgb": [183, 150, 248]},
-    "complementary_colour": {"hex": "#aae300", "rgb": [170, 227, 0]},
-    "triad_colour": {"hex": "#e33900", "rgb": [227, 57, 0]},
-    "text_colour": {"hex": "#e300aa", "rgb": [227, 0, 170]},
+    "background_colour": "#b796f8",
+    "complementary_colour": "#aae300",
+    "triad_colour": "#e33900",
+    "text_colour": "#e300aa",
     "number_font": {
         "filename": None,
         "font_size": 32,
@@ -35,23 +35,36 @@ class Theme:
 
     @staticmethod
     def from_dict(theme_dict: dict[str, str]) -> "Theme":
-        def _(font_dict: dict[str, str]) -> dict[str, str]:
+        def _(font_dict: dict[str, str], theme_dict: dict) -> dict[str, str]:
             """
             Helper function to set the colour of a font because we dont want repeated code (or json)
-            :param font_dict:
-            :return:
+            :param font_dict: The font dict to convert
+            :param ftype: The type of font to convert, either "n" for number, "t" for title, or "s" for subtitle
             """
+
             font_dict["colour"] = theme_dict["text_colour"]
             return font_dict
 
+        def __(colour_hex: str) -> Colour:
+            """
+            Helper function to set the rgb of a colour because we dont want repeated code (or json)
+            :param colour_hex: The hex of the colour to convert
+            :return: Colour
+            """
+            return Colour.from_hex(colour_hex)
+
+        print(theme_dict["complementary_colour"])
+        print(theme_dict["triad_colour"])
+        print(theme_dict["text_colour"])
+
         return Theme(
-            background_colour=Colour.from_dict(theme_dict["background_colour"]),
-            complementary_colour=Colour.from_dict(theme_dict["complementary_colour"]),
-            triad_colour=Colour.from_dict(theme_dict["triad_colour"]),
-            text_colour=Colour.from_dict(theme_dict["text_colour"]),
-            number_font=Font.from_dict(_(theme_dict["number_font"])),
-            title_font=Font.from_dict(_(theme_dict["title_font"])),
-            subtitle_font=Font.from_dict(_(theme_dict["subtitle_font"])),
+            background_colour=Colour.from_hex(theme_dict["background_colour"]),
+            complementary_colour=Colour.from_hex(theme_dict["complementary_colour"]),
+            triad_colour=Colour.from_hex(theme_dict["triad_colour"]),
+            text_colour=Colour.from_hex(theme_dict["text_colour"]),
+            number_font=Font.from_dict(_(theme_dict["number_font"], theme_dict)),
+            title_font=Font.from_dict(_(theme_dict["title_font"], theme_dict)),
+            subtitle_font=Font.from_dict(_(theme_dict["subtitle_font"], theme_dict)),
         )
 
     def to_dict(self) -> dict[str, str]:
