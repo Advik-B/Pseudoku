d406c1f62e8082d72e21b214d025b18205d167cc
Advik <advik.b@gmail.com>
2023-10-01 09:43:27 +0530

    Update __main__.py

--- pseudoku/__main__.py
+++ pseudoku/__main__.py
@@ -1,22 +1,2 @@
-import pygame
-
-pygame.init()
-pygame.font.init()
-
 from .mini3x3 import Mini3x3
 from .settings import Settings, load_settings, save_settings
-
-
-settings: Settings = load_settings()
-
-screen = pygame.display.set_mode((settings.width, settings.height))
-pygame.display.set_caption("Pseudoku")
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            save_settings(settings)
-            exit()
-    screen.fill(settings.theme.background_colour.rgb)
-    pygame.display.update()
