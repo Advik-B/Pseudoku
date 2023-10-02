f791350313f57408077ee99adbc7bb739c9a5544
Advik <advik.b@gmail.com>
2023-10-02 10:57:28 +0530

    Update pseudoku_backend.py

--- pseudoku/pseudoku_backend.py
+++ pseudoku/pseudoku_backend.py
@@ -10,22 +10,29 @@ class SudokuGrid:
         y: int = 0,
         screen: pygame.Surface = None,
         settings: Settings = None,
+        size: tuple[int, int] = None,
     ):
         self.matrix = [[x for x in range(9)] for y in range(9)]
         self.difficulty = difficulty
         self.screen = screen
         if not settings:
             self.settings = stngs.DEFAULT
+        else:
+            self.settings = settings
         self.font = settings.theme.number_font
         self.border_colour = settings.theme.complementary_colour
         self.border_width = 5
+        self.set_display(x, y, size)
+        self.generate_sudoku()
+        print(size)
+
+    def set_display(self, x: int, y: int, size: tuple[int, int] = None):
         self.x = x
         self.y = y
-        self.width = 25 * self.font.font_size + 2 * self.border_width
-        self.height = 25 * self.font.font_size + 2 * self.border_width
+        self.width = size[0] * self.font.font_size + 2 * self.border_width
+        self.height = size[1] * self.font.font_size + 2 * self.border_width
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.rect.center = (self.x, self.y)
-        self.generate_sudoku()
 
     def draw(self):
         pygame.draw.rect(
@@ -37,6 +44,9 @@ class SudokuGrid:
 
     def update(self):
         pass
+
+    def repos(self, x: int, y: int, size: tuple[int, int] = None):
+        self.set_display(x, y, size)
 
 
 if __name__ == "__main__":
