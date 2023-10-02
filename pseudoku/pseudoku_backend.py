59f38bbab4ca353f10f8818794e775ed8661595d
Advik <advik.b@gmail.com>
2023-10-02 10:28:17 +0530

    Update pseudoku_backend.py

--- pseudoku/pseudoku_backend.py
+++ pseudoku/pseudoku_backend.py
@@ -17,30 +17,20 @@ class SudokuGrid:
         if not settings:
             self.settings = stngs.DEFAULT
         self.font = settings.theme.number_font
-        self.border_colour = settings.theme.triad_colour
-        self.border_width = 2
+        self.border_colour = settings.theme.complementary_colour
+        self.border_width = 5
         self.x = x
         self.y = y
-        self.width = 9 * self.font.font_size + 2 * self.border_width
-        self.height = 9 * self.font.font_size + 2 * self.border_width
+        self.width = 25 * self.font.font_size + 2 * self.border_width
+        self.height = 25 * self.font.font_size + 2 * self.border_width
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.rect.center = (self.x, self.y)
         self.generate_sudoku()
 
     def draw(self):
-        pygame.draw.rect(self.screen, self.border_colour, self.rect, self.border_width)
-        for i in range(9):
-            for j in range(9):
-                if self.matrix[i][j] != 0:
-                    text = self.font.render(
-                        str(self.matrix[i][j]), self.border_colour, self.border_colour
-                    )
-                    text_rect = text.get_rect()
-                    text_rect.center = (
-                        self.x + (j + 0.5) * self.font.font_size,
-                        self.y + (i + 0.5) * self.font.font_size,
-                    )
-                    self.screen.blit(text, text_rect)
+        pygame.draw.rect(
+            self.screen, self.border_colour.rgb, self.rect, self.border_width
+        )
 
     def generate_sudoku(self):
         pass
