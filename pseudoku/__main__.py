8610a5c73b91f737519277b1617bc4fee3c29a02
Advik <advik.b@gmail.com>
2023-10-02 10:28:15 +0530

    Update __main__.py

--- pseudoku/__main__.py
+++ pseudoku/__main__.py
@@ -68,7 +68,12 @@ class Pseudoku:
             # Update the FPS text rect to be in the top left corner of the screen
             self.fps_text_rect = self.fps_text.get_rect(topleft=(10, 10))
 
-        self.grid = SudokuGrid(screen=self.screen, settings=self.settings)
+        self.grid = SudokuGrid(
+            screen=self.screen,
+            settings=self.settings,
+            x=self.screen_rect.centerx,
+            y=self.screen_rect.centery,
+        )
 
     def run(self):
         self.init()
@@ -97,6 +102,10 @@ class Pseudoku:
         self.clock.tick(self.settings.fps)
 
     def update(self):
+        # Check if the window has been resized
+        if self.settings.resizable and self.settings.size != self.screen.get_size():
+            self.resize(self.screen.get_size())
+
         if (
             self.settings.debug
         ):  # Update the caption if we are in debug mode to show the FPS
@@ -114,6 +123,17 @@ class Pseudoku:
     def quit(self):
         save_settings(self.settings)
         pygame.quit()
+
+    def resize(self, size: tuple[int, int]):
+        self.settings.size = size
+        self.screen = display.set_mode(self.settings.size, or_flags(flags))
+        self.screen_rect = self.screen.get_rect()
+        self.grid = SudokuGrid(
+            screen=self.screen,
+            settings=self.settings,
+            x=self.screen_rect.centerx,
+            y=self.screen_rect.centery,
+        )
 
 
 if __name__ == "__main__":
