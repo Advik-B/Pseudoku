ecea8791f913357d2e085351f3db08ccafc1f530
Advik <advik.b@gmail.com>
2023-11-29 12:13:31 +0530

    Update pseudoku_grid.py

--- pseudoku/pseudoku_grid.py
+++ pseudoku/pseudoku_grid.py
@@ -20,8 +20,10 @@ class SudokuGrid:
             self.settings = stngs.DEFAULT
         else:
             self.settings = settings
-        self.font = settings.theme.number_font
         self.border_colour = settings.theme.complementary_colour
+        self.num_font = settings.theme.number_font
+        print(self.num_font)
+        print(dir(self.num_font))
         self.border_width = 5
         self.set_display(x, y, size)
         self.generate_sudoku()
@@ -30,8 +32,8 @@ class SudokuGrid:
     def set_display(self, x: int, y: int, size: tuple[int, int] = None):
         self.x = x
         self.y = y
-        self.width = size[0] * self.font.font_size + 2 * self.border_width
-        self.height = size[1] * self.font.font_size + 2 * self.border_width
+        self.width = size[0] * self.num_font.font_size + 2 * self.border_width
+        self.height = size[1] * self.num_font.font_size + 2 * self.border_width
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.rect.center = (self.x, self.y)
 
@@ -100,6 +102,23 @@ class SudokuGrid:
                     ),
                     cell_border_width,
                 )
+
+        # Draw the numbers
+        for i in range(9):
+            for j in range(9):
+                if self.matrix[i][j] != 0:
+                    text = self.num_font.render(
+                        str(self.matrix[i][j]), self.settings.theme.background_colour, True
+                    )
+                    text_rect = text.get_rect(
+                        center=(
+                            self.rect.centerx - self.width // 2 + j * self.width // 9
+                            + self.width // 18,
+                            self.rect.centery - self.height // 2 + i * self.height // 9
+                            + self.height // 18,
+                        )
+                    )
+                    self.screen.blit(text, text_rect)
 
     def generate_sudoku(self):
         pass
